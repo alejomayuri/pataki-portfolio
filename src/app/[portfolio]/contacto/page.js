2d0d392918ad2a7553fef34ffd25dbed3d61245f
Alejo Mayuri <alejomayuri@gmail.com>
2025-04-04 19:26:02 -0500

    página contacto terminada

--- src/app/[portfolio]/contacto/page.js
+++ src/app/[portfolio]/contacto/page.js
@@ -5,28 +5,76 @@ import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
 export default function LinkTree() {
     const userData = {
         image: "/demo/persona1.jpg",
-        socialLinks: [
+        colections: [
             {
-                title: "Facebook",
-                link: "https://www.facebook.com/juanperezfotografia",
-                icon: <FaFacebook size={20} className="w-7 h-7" />,
+                title: "Contacto",
+                socialLinks: [
+                    {
+                        title: "Descargar CV",
+                        link: "/cv.pdf",
+                    },
+                    {
+                        title: "WhatsApp",
+                        link: "https://wa.me/1234567890",
+                    },
+                    {
+                        title: "Twitter",
+                        link: "https://twitter.com/juanperez_foto",
+                        image: "/demo/image2.jpg", // Ejemplo usando imagen en vez de icono
+                    },
+                    {
+                        title: "Instagram",
+                        link: "https://www.instagram.com/juanperezfotografia",
+                        icon: <FaInstagram size={20} className="w-7 h-7" />,
+                    },
+                    {
+                        title: "Linkedin",
+                        link: "https://www.linkedin.com/in/juanperezfotografia",
+                        image: "/demo/image3.jpg",
+                    },
+                ]
             },
             {
-                title: "Twitter",
-                link: "https://twitter.com/juanperez_foto",
-                image: "/demo/image2.jpg", // Ejemplo usando imagen en vez de icono
+                title: "Colaboraciones",
+                socialLinks: [
+                    {
+                        title: "Our Place x Alejo Mayuri",
+                        link: "https://www.ourplace.com/collections/alejo-mayuri",
+                        icon: <FaFacebook size={20} className="w-7 h-7" />,
+                    },
+                    {
+                        title: "Twitter",
+                        link: "https://twitter.com/juanperez_foto",
+                        image: "/demo/image2.jpg", // Ejemplo usando imagen en vez de icono
+                    },
+                    {
+                        title: "Serendipity x Alejo Mayuri",
+                        link: "https://www.linkedin.com/in/juanperezfotografia",
+                        image: "/demo/image3.jpg",
+                    },
+                ]
             },
             {
-                title: "Instagram",
-                link: "https://www.instagram.com/juanperezfotografia",
-                icon: <FaInstagram size={20} className="w-7 h-7" />,
-            },
-            {
-                title: "Linkedin",
-                link: "https://www.linkedin.com/in/juanperezfotografia",
-                image: "/demo/image3.jpg",
-            },
-        ]
+                title: "Ver",
+                socialLinks: [
+                    {
+                        title: "Stream Now Our Place x Alejo Mayuri",
+                        link: "https://www.ourplace.com/collections/alejo-mayuri",
+                        icon: <FaFacebook size={20} className="w-7 h-7" />,
+                    },
+                    {
+                        title: "Mirar Chef",
+                        link: "https://twitter.com/juanperez_foto",
+                        image: "/demo/image2.jpg", // Ejemplo usando imagen en vez de icono
+                    },
+                    {
+                        title: "Otras cosas",
+                        link: "https://www.linkedin.com/in/juanperezfotografia",
+                        image: "/demo/image3.jpg",
+                    },
+                ]
+            }
+        ],
     };
 
     return (
@@ -39,35 +87,42 @@ export default function LinkTree() {
                 />
                 <h1 className="text-lg font-bold mb-3 mt-2 m-auto">@alejomayuri</h1>
             </div>
-            <div className="text-center mb-8">
-                <h2 className="text-4xl font-bold mb-4">¡Conéctate conmigo!</h2>
-            </div>
 
             {/* Enlaces */}
             <div className="space-y-4">
-                {userData.socialLinks.map((link, index) => (
-                    <a 
-                        key={index} 
-                        href={link.link} 
-                        target="_blank" 
-                        rel="noopener noreferrer" 
-                        className="block text-center py-5 px-4 relative bg-gray-900 text-white rounded shadow-md hover:bg-gray-700 transition duration-300"
-                    >
-                        <div className="absolute left-3 top-1/2 transform -translate-y-1/2">
-                            {link.icon ? (
-                                link.icon
-                            ) : (
-                                <div className="w-11 h-11" >
-                                    <img 
-                                        src={link.image} 
-                                        alt={`${link.title} icon`} 
-                                        className="w-full h-full object-cover" 
-                                    />
-                                </div>
-                            )}
+                {userData.colections.map((colection, index) => (
+                    <div key={index} className="mb-8"> 
+                        <div className="text-center">
+                            <h2 className="text-lg font-bold mb-4">{colection?.title}</h2>
                         </div>
-                        {link.title}
-                    </a>
+                        <div className="flex flex-col gap-4">
+                            {colection.socialLinks.map((link, index) => (
+                                <div key={index}>
+                                    <a 
+                                        href={link.link} 
+                                        target="_blank" 
+                                        rel="noopener noreferrer" 
+                                        className="block text-center py-5 px-12 relative bg-gray-900 text-white rounded shadow-md hover:bg-gray-700 transition duration-300"
+                                    >
+                                        {(link.icon || link.image) && <div className="absolute left-3 top-1/2 transform -translate-y-1/2">
+                                            {link.icon ? (
+                                                link.icon
+                                            ) : (
+                                                <div className="w-11 h-11" >
+                                                    <img 
+                                                        src={link.image} 
+                                                        alt={`${link.title} icon`} 
+                                                        className="w-full h-full object-cover" 
+                                                    />
+                                                </div>
+                                            )}
+                                        </div>}
+                                        {link.title} 
+                                    </a>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
                 ))}
             </div>
         </div>
